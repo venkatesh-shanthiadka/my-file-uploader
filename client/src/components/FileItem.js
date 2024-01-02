d6ac238c445edc6bd115347e26a044e389d1554e
venkatesh.sv645 <venkatesh.sv645@gmail.com>
2024-01-02 16:45:49 +0000

    active item display

--- client/src/components/FileItem.js
+++ client/src/components/FileItem.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useParams, useNavigate } from 'react-router-dom';
 
 function FileItem({ file, index, pathtype }) {
+  let { fileindex } = useParams();
 
-  const onInputChecked  = (event) =>{
-  file.active = event.target.checked
+  const onInputChecked = (event) => {
+    file.active = event.target.checked
   }
 
   console.log('file: ', file);
@@ -13,12 +15,13 @@ function FileItem({ file, index, pathtype }) {
       <div className='col-2 d-flex'>
         <input type='checkbox' onChange={onInputChecked} />
       </div>
-      <div className='col-9 border-bottom pb-2'>
+      <div className={`col-9 border-bottom pb-2 ${parseInt(fileindex) === parseInt(index) ? 'text-light bg-dark' : ''}`}>
         <span className='px-2'>{index})</span>
-        <a style={{ 
-          color: 'white', 
+        <a style={{
+          color: 'white',
           fontWeight: 'bolder',
-          letterSpacing: '0.1rem' }} href={urlPath}>
+          letterSpacing: '0.1rem',
+        }} href={urlPath}>
           {file.name}
         </a>
       </div>
